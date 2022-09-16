626426f76efd8dfee541b1ff5c735a19ba4f3d06
tallurisanthisudha <talluri.santhisudha@zensark.com>
2022-09-16 12:08:37 +0530

    taskfour

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,8 +4,5 @@ class Post < ApplicationRecord
 
     has_one_attached :image
 
-    validate :image_presence
-  def image_presence
-    errors.add(:image, "can't be blank") unless image.attached?
-  end
+    
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,8 @@ class User < ApplicationRecord
          has_many :following_users, foreign_key: :followee_id, class_name: 'Follow'
          has_many :followers, through: :following_users
        
+         
+
          def follow(user)
            followed_users.create(followee_id: user.id)
          end
